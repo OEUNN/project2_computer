07a5929ba1dd252fe17f522d6a1355ec3de575d6
ys189389 <ys189389@gmail.com>
2022-12-12 20:21:46 +0900

    장바구니 삭제 에이젝스

--- WebContent/resources/javascript/basketUpdate.js
+++ WebContent/resources/javascript/basketUpdate.js
@@ -1,48 +1,35 @@
-$(document).ready(function(){
-	for(let i=0;i<3;i++){
-		appendBasket();
-		console.log("ready");
-	}
-});
-function appendBasket(){
-	let div = $(".basketTable table tbody");
-	//console.log(div);
-	let num=div.children().length;
-	let src="../resources/images/monitor.png";
-	let proName="컴퓨터";
-	let color="black";
-	let capacity="512GB";
-	let price=50000;
-	let qnt=2;
-	let totalPrice=$("#totalPrice span");
-	let rowPrice=$(".rowPrice hr");
-	$(".rowPrice h5>span").text(num);
-	div.append( "<tr class='basketRow'>"+
-						"<td>"+
-							"<div><input type='checkbox' id='basket"+num+"' name='basket"+num+"'/></div>"+
-						"</td>"+
-						"<td ><img src='"+src+"'/></td>"+
-						"<td><div >"+proName+"<span>("+color+","+capacity+" 택)</span></div></td>"+
-						"<td><div>"+price+"원</div></td>"+
-						"<td><div>"+qnt+"</div></td>"+	
-					"</tr>");	
-	rowPrice.before("<div id='row"+num+"Price'><span>"+price*qnt+"</span>원</div>");
-	
-	totalPrice.text(parseInt(totalPrice.text())+(price*qnt));
-	//console.log(div);
-	
-}
+
+
 function deleteBasket(){
-	let basketRow = $(".basketRow input:checked");
+	var checkArr = new Array();
+	$("input:checkbox[name='basket']").each(function(){
+		if($(this).is(":checked") == true){
+			checkArr.push($(this).val());
+			console.log($(this).val());
+		}
+	});
+	$.ajax({
+		url: "BasketDeleteController",
+		type: "post",
+		traditional: true,
+		data: {
+			checkedId : checkArr
+			
+		},
+		success: function(result){
+			$("#basketMain").empty();
+			$("#basketMain").html(result);
+			//$(".basketDetailOne").remove();
+			//$(".table").append(result);
 	
-	let basketNum=basketRow.attr("id").replace(/[^0-9]/g,"");
-	let totalPrice=$("#totalPrice span");
-	let rowPrice=$("#row"+basketNum+"Price");
-	//console.log(basketNum);
-	totalPrice.text(parseInt(totalPrice.text())-parseInt(rowPrice.children().text()));
-	rowPrice.remove();
-	basketRow.parent().parent().parent().remove();
-	$(".rowPrice h5 span").text($(".basketTable table tbody").length);
+			
+		}
+		
+		
+	});
+	
+	
+
 }
 
 function selectAll(){
--- src/servlet/product/BasketListController.java
+++ src/servlet/product/BasketListController.java
@@ -1,6 +1,8 @@
 package servlet.product;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,19 +12,32 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import Service.BasketService;
+import Service.ProductService;
 import dto.Basket;
+import dto.BasketDetail;
+import dto.Product;
  
-@WebServlet(name = "product.BasketListController", urlPatterns =  "/product/BasketListController" )
+@WebServlet(name = "product.BasketListController", urlPatterns = "/product/BasketListController" )
 public class BasketListController extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("BasketListController doGet()실행");
 		BasketService basketService = (BasketService) request.getServletContext().getAttribute("basketService");
+		ProductService productService = (ProductService) request.getServletContext().getAttribute("productService");
 		HttpSession session = request.getSession();
 		String userId = (String) session.getAttribute("loginId");
 		Basket basket = basketService.getBasket(userId);
 		int count = basketService.countBasketDetail(basket);
-		request.setAttribute("basket", basket);
+		List<BasketDetail> list = basket.getBasketDetail();
+		List<BasketDetail> myList = new ArrayList<>();
+		for(BasketDetail basketDetail : list) {
+			Product product = productService.getProduct(basketDetail.getProduct().getProductId());
+			basketDetail.setProduct(product);
+			myList.add(basketDetail);
+		}	
+		basket.setBasketDetail(myList);
+		//productService.getProduct(productId)
+		request.setAttribute("basket", basket);		
 		request.setAttribute("count", count);
 		
 		//JSP로 이동
