f0be68530d0eb1f8853614caa7e0d8284e1eb965
ys189389 <ys189389@gmail.com>
2022-12-15 19:03:45 +0900

    qna 예승 커밋

--- src/Service/QnaBoardService.java
+++ src/Service/QnaBoardService.java
@@ -11,100 +11,89 @@ import dto.QnaBoard;
 import util.Pager;
  
 public class QnaBoardService {
-	private ServletContext application;
-	private DataSource ds;
-	private QnaBoardDao qnaBoardDao;
-	
-	public QnaBoardService(ServletContext application) {
-		this.application=application;
-		qnaBoardDao = (QnaBoardDao)application.getAttribute("qnaBoardDao");
-		ds=(DataSource)application.getAttribute("dataSource");
-	}
-	
-	public int getTotalRow(String loginId) {
-		Connection conn = null;
-		int totalRow=0;
-		try {
-			conn=ds.getConnection();
-			totalRow = qnaBoardDao.getTotalRow(conn,loginId);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try{ conn.close(); }catch(Exception e) {}
-		}
-		return totalRow;
-	}
-	
-	public String writeQnaBoard(QnaBoard qnaBoard) {
-		Connection conn = null;
-		String Output=null;
-		try {
-			System.out.println("글쓰기 서비스");
-			conn=ds.getConnection();
-			Output = qnaBoardDao.Insert(qnaBoard,conn);
-			System.out.println(Output);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try{ conn.close(); }catch(Exception e) {e.printStackTrace();}
-		}
-		return Output;
-	}
-	
-	
-<<<<<<< HEAD
-	
-	
-	//o
-	public ArrayList<QnaBoard> readQnaBoardPager(Pager pager, String loginId) {
-=======
-	public ArrayList<QnaBoard> readQnaBoardPager(Pager pager) {
->>>>>>> 42a3130f16755fd8a026f9741cae5050f59d9941
-		Connection conn = null;
-		ArrayList<QnaBoard> list = new ArrayList<>();
-		
-		try {
-			conn=ds.getConnection();
-			list = qnaBoardDao.readQnaBoardPager(pager,conn,loginId);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try{ conn.close(); }catch(Exception e) {}
-		}
-		return list;
-	}
-	
-	
-	public String deleteQnaBoard(QnaBoard qnaBoard) {
-		Connection conn = null;
-		String Output=null;
-		try {
-			conn=ds.getConnection();
-			Output = qnaBoardDao.deleteQnaBoard(qnaBoard,conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try{ conn.close(); }catch(Exception e) {}
-		}
-		return Output;
-	}
-<<<<<<< HEAD
-	
-=======
->>>>>>> 42a3130f16755fd8a026f9741cae5050f59d9941
+   private ServletContext application;
+   private DataSource ds;
+   private QnaBoardDao qnaBoardDao;
+   
+   public QnaBoardService(ServletContext application) {
+      this.application=application;
+      qnaBoardDao = (QnaBoardDao)application.getAttribute("qnaBoardDao");
+      ds=(DataSource)application.getAttribute("dataSource");
+   }
+   
+   public int getTotalRow(String loginId) {
+      Connection conn = null;
+      int totalRow=0;
+      try {
+         conn=ds.getConnection();
+         totalRow = qnaBoardDao.getTotalRow(conn,loginId);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }finally {
+         try{ conn.close(); }catch(Exception e) {}
+      }
+      return totalRow;
+   }
+   
+   public String writeQnaBoard(QnaBoard qnaBoard) {
+      Connection conn = null;
+      String Output=null;
+      try {
+         System.out.println("글쓰기 서비스");
+         conn=ds.getConnection();
+         Output = qnaBoardDao.Insert(qnaBoard,conn);
+         System.out.println(Output);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }finally {
+         try{ conn.close(); }catch(Exception e) {e.printStackTrace();}
+      }
+      return Output;
+   }
+   
+   
+   public ArrayList<QnaBoard> readQnaBoardPager(Pager pager, String loginId) {
+      Connection conn = null;
+      ArrayList<QnaBoard> list = new ArrayList<>();
+      
+      try {
+         conn=ds.getConnection();
+         list = qnaBoardDao.readQnaBoardPager(pager,conn,loginId);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }finally {
+         try{ conn.close(); }catch(Exception e) {}
+      }
+      return list;
+   }
+   
+   
+   public String deleteQnaBoard(QnaBoard qnaBoard) {
+      Connection conn = null;
+      String Output=null;
+      try {
+         conn=ds.getConnection();
+         Output = qnaBoardDao.deleteQnaBoard(qnaBoard,conn);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }finally {
+         try{ conn.close(); }catch(Exception e) {}
+      }
+      return Output;
+   }
 
-	public String updateQnaBoard(QnaBoard qnaBoard) {
-		Connection conn = null;
-		String Output=null;
-		try {
-			conn=ds.getConnection();
-			Output = qnaBoardDao.updateQnaBoard(qnaBoard,conn);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}finally {
-			try{ conn.close(); }catch(Exception e) {}
-		}
-		return Output;
-	}
-	
+   public String updateQnaBoard(QnaBoard qnaBoard) {
+      Connection conn = null;
+      String Output=null;
+      try {
+         conn=ds.getConnection();
+         Output = qnaBoardDao.updateQnaBoard(qnaBoard,conn);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }finally {
+         try{ conn.close(); }catch(Exception e) {}
+      }
+      return Output;
+   }
+   
 }
--- src/dao/QnaBoardDao.java
+++ src/dao/QnaBoardDao.java
@@ -10,110 +10,97 @@ import dto.QnaBoard;
 import util.Pager;
 
 public class QnaBoardDao {
-	String Output;
-	int countRow;
+   String Output;
+   int countRow;
 
-	
-	public String Insert(QnaBoard qnaBoard, Connection conn) throws SQLException {
-		String sql = "insert into qna_board (QNA_BNO, QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID, IS_REPLY) "
-				+ " values ('Qna'||qna_bno.nextval, ?, ?, sysdate,  ? ,0) ";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		// 입력됨.
-		pstmt.setString(1, qnaBoard.getQnaBtitle());
-		pstmt.setString(2, qnaBoard.getQnaBcontent());
-		pstmt.setString(3, qnaBoard.getUserId());
-		pstmt.executeUpdate();
-		pstmt.close();
-		Output = "success";
+   
+   public String Insert(QnaBoard qnaBoard, Connection conn) throws SQLException {
+      String sql = "insert into qna_board (QNA_BNO, QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID, IS_REPLY) "
+            + " values ('Qna'||qna_bno.nextval, ?, ?, sysdate,  ? ,0) ";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      // 입력됨.
+      pstmt.setString(1, qnaBoard.getQnaBtitle());
+      pstmt.setString(2, qnaBoard.getQnaBcontent());
+      pstmt.setString(3, qnaBoard.getUserId());
+      pstmt.executeUpdate();
+      pstmt.close();
+      Output = "success";
 
-		return Output;
-	}
-
-<<<<<<< HEAD
-	
-	
-	public int getTotalRow(Connection conn, String loginId) throws SQLException {
-		String sql = "select count (*) as count from qna_board where user_id =? ";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, loginId);
-=======
+      return Output;
+   }
 
 
-	public int getTotalRow(Connection conn) throws SQLException {
-		String sql = "select count (*) as count from qna_board";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
->>>>>>> 42a3130f16755fd8a026f9741cae5050f59d9941
-		ResultSet rs = pstmt.executeQuery();
-		while (rs.next()) {
-			countRow = rs.getInt("count");
-		}
-		pstmt.close();
-		rs.close();
 
-		return countRow;
-	}
+   public int getTotalRow(Connection conn, String loginId) throws SQLException {
+      String sql = "select count (*) as count from qna_board where user_id =? ";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, loginId);
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+         countRow = rs.getInt("count");
+      }
+      pstmt.close();
+      rs.close();
 
-	public ArrayList<QnaBoard> readQnaBoardPager(Pager pager, Connection conn, String loginId) throws SQLException {
-		ArrayList<QnaBoard> list = new ArrayList<>();
-		String sql = "select rnum, QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID,  QNA_BNO,IS_REPLY " 
-				+ " from ( "
-				+ " select rownum as rnum, QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID,  QNA_BNO, IS_REPLY "
-				+ " from (select QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID,  QNA_BNO, IS_REPLY " 
-				+ " from qna_board where user_id=? "
-				+ " order by QNA_DATE desc " + " ) where rownum <= ? " + " )" + " where rnum >= ? ";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1,loginId);
-		pstmt.setInt(2, pager.getEndRowNo());
-		pstmt.setInt(3, pager.getStartRowNo());
-		ResultSet rs = pstmt.executeQuery();
-		while (rs.next()) {
-			QnaBoard qna_Board = new QnaBoard();
-			qna_Board.setQnaBtitle(rs.getString(2));
-			qna_Board.setQnaBcontent(rs.getString(3));
-			qna_Board.setQnaDate(rs.getString(4));
-			qna_Board.setUserId(rs.getString(5));
-			qna_Board.setQnaBno(rs.getString(6));
-			qna_Board.setReply(rs.getBoolean(7));
-			list.add(qna_Board);
-		}
-		pstmt.close();
-		rs.close();
+      return countRow;
+   }
 
-		return list;
-	}
+   public ArrayList<QnaBoard> readQnaBoardPager(Pager pager, Connection conn, String loginId) throws SQLException {
+      ArrayList<QnaBoard> list = new ArrayList<>();
+      String sql = "select rnum, QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID,  QNA_BNO,IS_REPLY " 
+            + " from ( "
+            + " select rownum as rnum, QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID,  QNA_BNO, IS_REPLY "
+            + " from (select QNA_BTITLE, QNA_BCONTENT, QNA_DATE, USER_ID,  QNA_BNO, IS_REPLY " 
+            + " from qna_board where user_id=? "
+            + " order by QNA_DATE desc " + " ) where rownum <= ? " + " )" + " where rnum >= ? ";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1,loginId);
+      pstmt.setInt(2, pager.getEndRowNo());
+      pstmt.setInt(3, pager.getStartRowNo());
+      ResultSet rs = pstmt.executeQuery();
+      while (rs.next()) {
+         QnaBoard qna_Board = new QnaBoard();
+         qna_Board.setQnaBtitle(rs.getString(2));
+         qna_Board.setQnaBcontent(rs.getString(3));
+         qna_Board.setQnaDate(rs.getString(4));
+         qna_Board.setUserId(rs.getString(5));
+         qna_Board.setQnaBno(rs.getString(6));
+         qna_Board.setReply(rs.getBoolean(7));
+         list.add(qna_Board);
+      }
+      pstmt.close();
+      rs.close();
 
-<<<<<<< HEAD
+      return list;
+   }
 
+   
 
-=======
-	
->>>>>>> 42a3130f16755fd8a026f9741cae5050f59d9941
+   public String deleteQnaBoard(QnaBoard qnaBoard, Connection conn) throws SQLException {
+      String answer = "";
+      String sql = "delete from qna_board where qna_bno = ? ";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      
+      pstmt.setString(1, qnaBoard.getQnaBno());
+      pstmt.executeUpdate();
+      answer = "success";
+      pstmt.close();
 
-	public String deleteQnaBoard(QnaBoard qnaBoard, Connection conn) throws SQLException {
-		String answer = "";
-		String sql = "delete from qna_board where qna_bno = ? ";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		
-		pstmt.setString(1, qnaBoard.getQnaBno());
-		pstmt.executeUpdate();
-		answer = "success";
-		pstmt.close();
+      return answer;
+   }
+   
 
-		return answer;
-	}
-	
+   public String updateQnaBoard(QnaBoard qnaBoard, Connection conn)throws Exception {
+      String answer = "";
+      String sql = "update qna_board set qna_btitle = ? , qna_bcontent = ? where qna_bno = ? ";
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, qnaBoard.getQnaBtitle());
+      pstmt.setString(2, qnaBoard.getQnaBcontent());
+      pstmt.setString(3, qnaBoard.getQnaBno());
+      pstmt.executeUpdate();
+      answer = "success";
+      pstmt.close();
 
-	public String updateQnaBoard(QnaBoard qnaBoard, Connection conn)throws Exception {
-		String answer = "";
-		String sql = "update qna_board set qna_btitle = ? , qna_bcontent = ? where qna_bno = ? ";
-		PreparedStatement pstmt = conn.prepareStatement(sql);
-		pstmt.setString(1, qnaBoard.getQnaBtitle());
-		pstmt.setString(2, qnaBoard.getQnaBcontent());
-		pstmt.setString(3, qnaBoard.getQnaBno());
-		pstmt.executeUpdate();
-		answer = "success";
-		pstmt.close();
-
-		return answer;
-	}
+      return answer;
+   }
 }
